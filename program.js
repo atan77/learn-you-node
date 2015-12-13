e34824e2ec94381d711156213ff7e159624ed94e
Ashley <atan77@gmail.com>
2015-12-13 17:46:34 +1100

    2 Baby steps
    Write a program that accepts one or more numbers as command-line
    arguments
      and prints the sum of those numbers to the console
    (stdout).

--- program.js
+++ program.js
@@ -1,4 +1,6 @@
+//variable to sum arguments passed to function
 var accum=0;
+//loop to iterate through arguments
 for (var i=2; i<process.argv.length; i++) {
     accum=accum+Number(process.argv[i]);
 }

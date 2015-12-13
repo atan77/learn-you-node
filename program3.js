3c200cc10c9fdaf559e099ff7eaa231062608d1a
Ashley <atan77@gmail.com>
2015-12-13 17:11:16 +1100

    3 My first I/O task
    Access a file from the filesystem and return a count of the number of
    newlines.

--- program3.js
+++ program3.js
@@ -1,4 +1,7 @@
+//required for code to access the filesystem
 var fs = require('fs')
+//process.argv[2] is the filename passed to the function
+//split on \n for regex search for new lines, this creates an array which will be one element too long as the last element won't have a new line attached
 var newLineCount = fs.readFileSync(process.argv[2]).toString().split('\n').length-1;
 
 console.log(newLineCount);

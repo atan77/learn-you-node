0079b8d28f47c349d8789ad0586277813067cba2
Ashley <atan77@gmail.com>
2015-12-14 16:45:34 +1100

    Update program5.js

--- program5.js
+++ program5.js
@@ -1,14 +1,12 @@
 var fs = require('fs');
 var path = require('path');
-var dirname = process.argv[2];
-var ext = process.argv[3];
 
 //returns directory path
 //directory = process.argv[2];
 //    console.log(arguments);
 
 
-var    dirContents = fs.readdir(dirname, function (err, dirlist) {
+var    dirContents = fs.readdir(process.argv[2], function (err, dirlist) {
         //response if error occurs
         if (err) throw err;
         //required output for console, toString converts buffer output to string, then split and count number of array elements remembering that the last element does not have a new line

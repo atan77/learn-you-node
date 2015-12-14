24d705b104a2dc4d0d3316a99535e4fd54e2e7ed
Ashley <atan77@gmail.com>
2015-12-14 15:13:14 +1100

    4 My first async IO
    Removed unnecessary code.  Original code was more generic.

--- program4.js
+++ program4.js
@@ -1,18 +1,19 @@
 //enable filesystem access
 var fs = require('fs');
-
+var newLineCount = undefined;
 //function to perform filesystem methods
-function strExtract(callback) {
+function strExtract() {
     fs.readFile(process.argv[2], function doneReading(err, fileContents) {
         //response if error occurs
         if (err) throw err;
         //required output for console, toString converts buffer output to string, then split and count number of array elements remembering that the last element does not have a new line
-        console.log(fileContents.toString().split('\n').length-1);
-        callback();
+        newLineCount = fileContents.toString().split('\n').length-1;
+        console.log(newLineCount);
+//        doneReading();
     });
 }
 //    function newLineCount() {
 //        console.log();
-    }
+//    }
     
-    strExtract(newLineCount);
+    strExtract();
